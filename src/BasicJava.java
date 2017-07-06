d0b2481ad8fa682678893bb0bb339424967b4a31
PavelDT <paveldt2@gmail.com>
2017-07-06 04:54:16 -0700

    added java for loop and for each loop

--- src/BasicJava.java
+++ src/BasicJava.java
@@ -1,3 +1,5 @@
+import java.util.List;
+import java.util.ArrayList;
 
 public class BasicJava {
     Range age = new Range(5, 10);
@@ -10,6 +12,31 @@ public class BasicJava {
     // this is the entry point for BasicJava
     public static void main(String[] args)
     {
-        new BasicJava().inspectRancge();
+        BasicJava baseJava = new BasicJava();
+        // baseJava.inspectRancge();
+        BasicJava.countTo5();
+        baseJava.printCollection();
+    }
+
+    public static void countTo5()
+    {
+        for (int i = 1; i < 11; i++)
+        {
+            String message = "this is a simple counter " + i;
+            System.out.println(message);
+        }
+    }
+
+    public void printCollection()
+    {
+        List<Integer> l = new ArrayList<Integer>();
+        l.add(3);
+        l.add(4);
+        l.add(-31);
+
+        for(int a : l)
+        {
+            System.out.println("Print variable in list " + a);
+        }
     }
 }
